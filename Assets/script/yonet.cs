2ddb5fea5a1b1b3b831bb17bd15e136162af244c
Oguzhan Secgel <oguzhansecgel19@icloud.com>
2023-05-24 15:54:27 +0300

    Second Commit
    The Photon framework was installed and the code for server, lobby, and room entry was added.

---
+++ Assets/script/yonet.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+public class yonet : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("servera girildi");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("lobiye girildi");
+        PhotonNetwork.JoinOrCreateRoom("oda",new RoomOptions { MaxPlayers =2,IsOpen=true,IsVisible=true},TypedLobby.Default);
+        
+
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("odaya girildi");
+
+    }
+    public override void OnLeftLobby()
+    {
+        Debug.Log("lobiden çýkýldý");
+    }
+    public override void OnLeftRoom()
+    {
+        Debug.Log("odadan çýkýldý");
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Hata : Odaya Girilemedi..!");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Hata : Herhangi Bir Odaya Girilemedi..!");
+
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Hata : Oda Kurulamadý..!");
+    }
+}
